435d21ca3d78d31ecf2b040ac02182aa1221898e
vishal pranav <vbvishalpranav@gmail.com>
2024-12-22 13:45:15 +0530

    removed the comment

--- src/main/java/com/vb/bill/BillingManagementSystemApplication.java
+++ src/main/java/com/vb/bill/BillingManagementSystemApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BillingManagementSystemApplication {
-// This billing Application
-    public static void main(String[] args) {
-        SpringApplication.run(BillingManagementSystemApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingManagementSystemApplication.class, args);
+	}
 
 }
