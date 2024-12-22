14b5ba6f2d91a14106f9b5923966cb01d9e459bc
vishal pranav <vbvishalpranav@gmail.com>
2024-12-22 13:36:50 +0530

    Added comment message

--- src/main/java/com/vb/bill/BillingManagementSystemApplication.java
+++ src/main/java/com/vb/bill/BillingManagementSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BillingManagementSystemApplication {
-
+// This billing Application
     public static void main(String[] args) {
         SpringApplication.run(BillingManagementSystemApplication.class, args);
     }
